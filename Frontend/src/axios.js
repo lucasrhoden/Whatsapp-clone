1e4c88775b1dba6a4492f63996c329558fedebf8
Lucas Rhoden <62027315+lucasrhoden@users.noreply.github.com>
2020-09-10 11:04:33 +0100

    adjusted axios.js

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: `http://localhost:${process.env.PORT || "9000"}`
+    baseUrl = process.env.baseURL || "http://localhost:9000"
 })
 
 export default instance;
