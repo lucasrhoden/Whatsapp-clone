5060540e863899d90141433de2e6657c6bb798bc
Lucas Rhoden <62027315+lucasrhoden@users.noreply.github.com>
2020-09-10 12:03:11 +0100

    added heruku server

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9000' || process.env.baseURL
+    baseURL: 'http://localhost:9000' || process.env.baseURL || "https://rocky-taiga-88829.herokuapp.com"
 })
 
 export default instance;
