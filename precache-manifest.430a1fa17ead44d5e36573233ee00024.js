683616e7fd59078f42741f1d677b1914467ee107
Lucas Rhoden <62027315+lucasrhoden@users.noreply.github.com>
2020-09-10 10:38:44 +0100

    Updates

---
+++ precache-manifest.430a1fa17ead44d5e36573233ee00024.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc07b4e93c7820d5ddd197b77ad2fbd",
+    "url": "/Whatsapp-clone/index.html"
+  },
+  {
+    "revision": "32a8fa5fadb1a2598948",
+    "url": "/Whatsapp-clone/static/css/main.43cfcbe7.chunk.css"
+  },
+  {
+    "revision": "cb6b9915f36e3fa0077b",
+    "url": "/Whatsapp-clone/static/js/2.392981b6.chunk.js"
+  },
+  {
+    "revision": "95e5393dbc94b774bee63119a26602ef",
+    "url": "/Whatsapp-clone/static/js/2.392981b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a8fa5fadb1a2598948",
+    "url": "/Whatsapp-clone/static/js/main.1e27c1a7.chunk.js"
+  },
+  {
+    "revision": "9b1bdb5dfe8f5ef4e051",
+    "url": "/Whatsapp-clone/static/js/runtime-main.b52a364d.js"
+  }
+]);
